a56d0c20993fb42b21c3fec11d752db628794cfd
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-06-11 16:25:09 +0100

    List of usefull selections for Inv Higgs

---
+++ Selection.h
@@ -0,0 +1,39 @@
+#ifndef SELECTION_h
+#define SELECTION_h
+
+#include <iostream>
+
+// Here go the offline cuts:
+//    pT(j1),pT(j2) > 50 GeV
+//    |eta| < 4.7
+//    eta(j1),eta(j2) < 0
+//    Delta(eta_jj) > 4.2
+//    Di-jet mass > 1100 GeV
+//    MET > 130 GeV
+//    Delta(phi_jj) < 1 radians
+//    CJV to any event with additional jet pT > 30 GeV
+
+class Selection{
+
+ public:
+  
+  static float GetEntries() { return 1000; }
+  
+  static float NJets() { return 2; } 
+  
+  static float JetPTCut() { return 50; }
+  
+  static float JetEtaCut() { return 4.7; } 
+  
+  static float JetMassesCut() { return 1100; }
+  
+  static float DeltaPhiJJCut() { return 1; } 
+  
+  static float DeltaEtaJJCut() { return 4.2; } 
+  
+  static float METCut() { return 130; }
+  
+
+};
+
+#endif
