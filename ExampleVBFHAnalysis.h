4c1ec36e9915d160ca85d96e2d2b00befd151222
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-06-18 15:04:34 +0100

    changes

--- ExampleVBFHAnalysis.h
+++ ExampleVBFHAnalysis.h
@@ -534,9 +534,9 @@ ExampleVBFHAnalysis::ExampleVBFHAnalysis(TTree *tree) : fChain(0)
 {
 
    if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("event_vbf_inv_8000_output.root");
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("hepmcoutputUPDATE.root");
       if (!f || !f->IsOpen()) {
-         f = new TFile("event_vbf_inv_8000_output.root");
+         f = new TFile("hepmcoutputUPDATE.root");
       }
       f->GetObject("Delphes",tree);
 
