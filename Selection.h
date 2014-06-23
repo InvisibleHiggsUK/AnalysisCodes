98321638415ac789c88dfa9a2b3604c4dcfcae58
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-06-23 18:27:24 +0100

    changes

--- Selection.h
+++ Selection.h
@@ -13,12 +13,29 @@
 //    Delta(phi_jj) < 1 radians
 //    CJV to any event with additional jet pT > 30 GeV
 
+
 class Selection{
 
  public:
   
   //static float GetEntries() { return 1000; }
+
+  static bool JetCut(float pt){ return pt > 50.; }
   
+  static bool EtaCut(float eta){ return abs(eta) < 4.7; }
+
+  static bool PhiCut(float phi){ return phi < 1; }
+
+  static bool METcut(float met){ return met > 130; }
+
+  static bool MassCut(float m_jj){ return m_jj > 1100; }
+
+  static bool DEtaCut(float dEtajj){ return dEtajj > 4.2; }
+
+  static bool DPEta(float etaDP){ return etaDP < 0; }
+
+
+
   static float NJets() { return 2; } 
   
   static float JetPTCut() { return 50; }
@@ -35,6 +52,8 @@ class Selection{
   
   static float METCut() { return 150; }
   
+  //  extern  Float_t pi = 3.14159265;
+
 
 };
 
