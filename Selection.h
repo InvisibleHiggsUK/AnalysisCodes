c06978ee81eb8d8e0b94b53eb73e68eaddfec7f4
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-06-24 16:00:04 +0100

    including rough trigger cuts

--- Selection.h
+++ Selection.h
@@ -20,6 +20,11 @@ class Selection{
   
   //static float GetEntries() { return 1000; }
 
+  static bool TriggerCuts(float jetpt1, float jetpt2, float eta, float dEtajj, float mjj, float met)
+  {
+    return (jetpt1 > 40 && jetpt2 > 40 && eta < 0 && dEtajj > 3.5 && mjj > 800 && met > 65);
+  }
+
   static bool JetCut(float pt){ return pt > 50.; }
   
   static bool EtaCut(float eta){ return abs(eta) < 4.7; }
@@ -33,6 +38,8 @@ class Selection{
   static bool DEtaCut(float dEtajj){ return dEtajj > 4.2; }
 
   static bool DPEta(float etaDP){ return etaDP < 0; }
+
+  static bool CJVCut(float eta1, float eta2, float eta3, float pt3){ return (eta1 < eta3 && eta3 < eta2 && pt3 > 30) || (eta2 < eta3 && eta3 < eta1 && pt3 > 30); }
 
 
 
