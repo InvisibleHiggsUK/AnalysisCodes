5f7b8a5d33b4d596e1c71e17db351b1194b53608
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-06-29 16:38:00 +0100

    needs work

--- NtupleAnalysis/AnalyseNtuples.C
+++ NtupleAnalysis/AnalyseNtuples.C
@@ -14,13 +14,25 @@ void AnalyseNtuples(unsigned int id, int nEvts = -1){
   
   std::cout << "Analysing the '"<< Sample::toTString(id) << "' sample" << std::endl;
 
+  TH1* hNJets = new TH1D("hNJets","hNJets",12,0,12);
+  hNJets->Sumw2();
+  TH1* hJet1PT = new TH1D("hJet1PT","hJetPT",100, 0, 300);
+  hJet1PT->Sumw2();
+  TH1* hJet1Eta = new TH1D("hJet1Eta","hJetEta",50, -10, 10);
+  hJet1Eta->Sumw2();
+  TH1* hJet2PT = new TH1D("hJet2PT","hJet2PT",100, 0, 300);
+  hJet2PT->Sumw2();
+  TH1* hJet2Eta = new TH1D("hJet2Eta","hJet2Eta",50, -10, 10);
+  hJet2Eta->Sumw2();
+  
+    
 
   Event* evt = new Event(Sample::fileNameFullSample(id),nEvts);
 
   while( evt->loadNext() ) {
 
     std::cout << "NEW ENTRY" << std::endl;
-    std::cout << "Jet PT: " << evt->JET1PT()[1] << std::endl;
+    std::cout << "Jet PT: " << evt->JET1PT()[0] << std::endl;
 
   }
 
